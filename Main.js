b94baf467decd0f90af6583fc75d36afef02a483
Mampuru <mampuruntwampe@gmail.com>
2022-08-23 23:13:19 +0200

    solution one and two

---
+++ Main.js
@@ -0,0 +1,20 @@
+// SOLUTION A
+var introductionMesssage1 =  "My Name is Ntwampe"
+introductionMesssage1 +=  ", I'm a student at MTN App Academy"
+introductionMesssage1 += " my Github username is NMampuru"
+
+console.log(introductionMesssage1);
+
+// SOLUTION B
+var appCat = ["Best Enterprise Solution","Best Consumer Solution","Best Gaming Solution","Best African Solution","Most Innovative Solution","Best Incubated Solution","Best Educational Solution","Best Agricultural Solution","Best Financial Solution","Best Health Solution","Best Breakthrough Developer","Best South African Solution"," Campus Cup","Huawei AppGallery Category 15"]
+
+var dice = Math.random() * 13;
+let number = Math.round(dice)
+
+console.log("Dice roll")
+console.log(number)
+
+console.log("*******Winning category for MTN Business App of the Year in the year 2022*******")
+console.log(appCat[number])
+
+// SOLUTION C
